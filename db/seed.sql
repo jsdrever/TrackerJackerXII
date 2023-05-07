ec07cb4d964fb43cb62b2fd9ea42553ea9485101
jsdrever <jsdrever09@gmail.com>
2023-05-07 12:53:02 -0500

    updated a bunch of code trying something new here

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,36 @@
+-- use employees;
+
+-- INSERT INTO department
+--     (name)
+-- VALUES
+--     ('Sales'),
+--     ('Engineering'),
+--     ('Finance'),
+--     ('Legal');
+
+-- INSERT INTO role
+--     (title, salary, department_id)
+-- VALUES
+--     ('Sales Lead', 100000, 1),
+--     ('Salesperson', 80000, 1),
+--     ('Lead Engineer', 150000, 2),
+--     ('Software Engineer', 120000, 2),
+--     ('Account Manager', 160000, 3),
+--     ('Accountant', 125000, 3),
+--     ('Legal Team Lead', 250000, 4),
+--     ('Lawyer', 190000, 4);
+
+-- INSERT INTO employee
+--     (first_name, last_name, role_id, manager_id)
+-- VALUES
+--     ('Jeff', 'Winger', 1, NULL),
+--     ('Shirley', 'Bennett', 2, 1),
+--     ('Annie', 'Edison', 3, NULL),
+--     ('Pierce', 'Hawthorn', 4, 3),
+--     ('Troy', 'Barnes', 5, NULL),
+--     ('Abed', 'Nadir', 6, 5),
+--     ('Britta', 'Perry', 7, NULL),
+--     ('Ben', 'Chang', 8, 7);
 use employees;
 
 INSERT INTO department
@@ -23,11 +56,11 @@ VALUES
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Jeff', 'Winger', 1, NULL),
-    ('Shirley', 'Bennett', 2, 1),
-    ('Annie', 'Edison', 3, NULL),
-    ('Pierce', 'Hawthorn', 4, 3),
-    ('Troy', 'Barnes', 5, NULL),
-    ('Abed', 'Nadir', 6, 5),
-    ('Britta', 'Perry', 7, NULL),
-    ('Ben', 'Chang', 8, 7);
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
